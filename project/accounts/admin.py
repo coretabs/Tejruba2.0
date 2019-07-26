7d7218e09822f800adf83258806044da5d57f1ad
Ibrahim Badwi <ib.a.badwi@gmail.com>
2019-07-26 18:12:23 +0300

    change accounts model

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 from .models import Profile, SocialAccount
 
@@ -7,9 +9,21 @@ class SocialAccountInline(admin.TabularInline):
     model = SocialAccount
 
 
+class ProfileInline(admin.TabularInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+
 class ProfileAdmin(admin.ModelAdmin):
     inlines = [SocialAccountInline, ]
 
 
+# admin.site.unregister(User)
+# admin.site.register(User, CustomUserAdmin)
 admin.site.register(Profile, ProfileAdmin)
-admin.site.register(SocialAccount)
--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
-    bio = models.TextField()
-    birth_date = models.DateTimeField(auto_now_add=True)
-    country = models.CharField(max_length=100)
-    job = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+    birth_date = models.DateTimeField(null=True, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+    job = models.CharField(max_length=100, blank=True)
     # add photo field
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, default="")
@@ -29,3 +31,14 @@ class SocialAccount(models.Model):
 
     def __str__(self):
         return f"<Social Account: {self.account_type}>"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
